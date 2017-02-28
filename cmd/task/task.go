2244ed3e5b1d49ea9a40996d3e3b8f384bb515da
Travis <tscholl2@uw.edu>
2017-02-28 08:40:17 -0800

    add toml example, use yaml in help doc

--- cmd/task/task.go
+++ cmd/task/task.go
@@ -15,17 +15,12 @@ task [target1 target2 ...]: Runs commands under targets like make.
 Example: 'task hello' with the following 'Taskfile.json' file will generate
 an 'output.txt' file.
 '''
-{
-  "hello": {
-    "cmds": [
-      "echo \"I am going to write a file named 'output.txt' now.\"",
-      "echo \"hello\" > output.txt"
-    ],
-    "generates": [
-      "output.txt"
-    ]
-  }
-}
+hello:
+  cmds:
+    - echo "I am going to write a file named 'output.txt' now."
+    - echo "hello" > output.txt
+  generates:
+    - output.txt
 '''
 `)
 	}
