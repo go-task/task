f4216dd67f136e177df55721e6c70a33136b2dd4
Andrey Nering <andrey.nering@gmail.com>
2017-11-02 09:45:00 -0200

    list tasks: ensure at least one space after colon

--- help.go
+++ help.go
@@ -17,7 +17,7 @@ func (e *Executor) PrintTasksHelp() {
 	// Format in tab-separated columns with a tab stop of 8.
 	w := tabwriter.NewWriter(e.Stdout, 0, 8, 0, '\t', 0)
 	for _, task := range tasks {
-		fmt.Fprintln(w, fmt.Sprintf("* %s:\t%s", task, e.Tasks[task].Desc))
+		fmt.Fprintln(w, fmt.Sprintf("* %s: \t%s", task, e.Tasks[task].Desc))
 	}
 	w.Flush()
 }
