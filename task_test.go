00a0755ff3f4c96932c166f0821678e7e2d9a1be
Josh Bebbington <josh.bebbington92@gmail.com>
2018-07-31 23:09:55 +0100

    Added test for executor.DryRun

--- task_test.go
+++ task_test.go
@@ -432,3 +432,25 @@ func TestExpand(t *testing.T) {
 	assert.NoError(t, e.Run(taskfile.Call{Task: "pwd"}))
 	assert.Equal(t, home, strings.TrimSpace(buff.String()))
 }
+
+func TestDryRun(t *testing.T) {
+	const dir = "testdata/dryrun"
+
+	file := filepath.Join(dir, "file.txt")
+	_ = os.Remove(file)
+
+	var buff bytes.Buffer
+
+	e := task.Executor{
+		Dir:    dir,
+		Stdout: &buff,
+		Stderr: &buff,
+		DryRun: true,
+	}
+	assert.NoError(t, e.Setup())
+	assert.NoError(t, e.Run(taskfile.Call{Task: "build"}))
+
+	if _, err := os.Stat(file); err == nil {
+		t.Errorf("File should not exist %s", file)
+	}
+}
