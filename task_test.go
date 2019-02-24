1c3082ffa6cc0a5a72f897bf1ff5783f4b53aa9f
jaedle <dennis.jekubczyk@gmail.com>
2019-02-24 14:48:48 +0100

    rename test fixture

--- task_test.go
+++ task_test.go
@@ -580,7 +580,7 @@ func TestDetails(t *testing.T) {
 	}
 	assert.NoError(t, e.Setup())
 	assert.NoError(t, e.Run(context.Background(), taskfile.Call{Task: "task-with-details"}))
-	assert.Equal(t, readTestFixture(t, dir, "task-with-details-details.txt"), buff.String())
+	assert.Equal(t, readTestFixture(t, dir, "task-with-details.txt"), buff.String())
 
 	buff.Reset()
 	const noDetails = "task-without-details"
