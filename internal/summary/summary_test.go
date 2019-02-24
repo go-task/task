474f27c6d3922ec77a2ebdc68696aeeef6300c47
jaedle <dennis.jekubczyk@gmail.com>
2019-02-24 17:10:59 +0100

    add unit test for displaying commands

--- internal/summary/summary_test.go
+++ internal/summary/summary_test.go
@@ -9,7 +9,7 @@ import (
 	"testing"
 )
 
-func TestPrintsDependencies(t *testing.T) {
+func TestPrintsDependenciesIfPresent(t *testing.T) {
 	buffer := &bytes.Buffer{}
 	l := logger.Logger{
 		Stdout:  buffer,
@@ -32,7 +32,7 @@ func TestPrintsDependencies(t *testing.T) {
 	assert.Contains(t, buffer.String(), "\n - dep3\n")
 }
 
-func TestDoesNotPrintDependencies(t *testing.T) {
+func TestDoesNotPrintDependenciesIfMissing(t *testing.T) {
 	buffer := &bytes.Buffer{}
 	l := logger.Logger{
 		Stdout:  buffer,
@@ -46,4 +46,43 @@ func TestDoesNotPrintDependencies(t *testing.T) {
 	summary.Print(&l, task)
 
 	assert.NotContains(t, buffer.String(), "dependencies:")
+}
+
+func TestPrintTaskName(t *testing.T) {
+	buffer := &bytes.Buffer{}
+	l := logger.Logger{
+		Stdout:  buffer,
+		Stderr:  buffer,
+		Verbose: false,
+	}
+	task := &taskfile.Task{
+		Task: "my-task-name",
+	}
+
+	summary.Print(&l, task)
+
+	assert.Contains(t, buffer.String(), "task: my-task-name\n")
+}
+
+func TestPrintTaskCommandsIfPresent(t *testing.T) {
+	buffer := &bytes.Buffer{}
+	l := logger.Logger{
+		Stdout:  buffer,
+		Stderr:  buffer,
+		Verbose: false,
+	}
+	task := &taskfile.Task{
+		Cmds: []*taskfile.Cmd{
+			{Cmd: "command-1"},
+			{Cmd: "command-2"},
+			{Cmd: "command-3"},
+		},
+	}
+
+	summary.Print(&l, task)
+
+	assert.Contains(t, buffer.String(), "\ncommands:\n")
+	assert.Contains(t, buffer.String(), "\n - command-1\n")
+	assert.Contains(t, buffer.String(), "\n - command-2\n")
+	assert.Contains(t, buffer.String(), "\n - command-3\n")
 }
