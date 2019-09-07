d8005b4cf6ced4f83e316b155f3a4f9e5d2563f9
Andrey Nering <andrey.nering@gmail.com>
2019-09-07 14:32:02 -0300

    Fix typo

--- internal/homefix/homefix.go
+++ internal/homefix/homefix.go
@@ -1,5 +1,5 @@
 // Package homefix exists to address a bug where mvdan.cc/sh expects
 // $HOME to be available in order to be able to expand "~".
 //
-// This should delete this package once this is fixed there.
+// We should delete this package once this is fixed there.
 package homefix
