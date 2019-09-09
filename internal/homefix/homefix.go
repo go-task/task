0e1d8a72e6d00029362b8e24d35853202d86a724
Andrey Nering <andrey.nering@gmail.com>
2019-09-08 21:56:23 -0300

    Revert "Ensure the $HOME env is being set on Windows"
    This reverts commit 52028fc3bc5c9a80d3974b2f74efa491229df075.

--- internal/homefix/homefix.go
+++
@@ -1,5 +0,0 @@
-// Package homefix exists to address a bug where mvdan.cc/sh expects
-// $HOME to be available in order to be able to expand "~".
-//
-// We should delete this package once this is fixed there.
-package homefix
