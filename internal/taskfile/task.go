b7bcd204b4f240e1b3b9bb7bebbc7e70e494d4be
Andrey Nering <andrey.nering@gmail.com>
2019-12-07 20:09:16 -0300

    go fmt internal/taskfile/task.go

--- internal/taskfile/task.go
+++ internal/taskfile/task.go
@@ -5,20 +5,20 @@ type Tasks map[string]*Task
 
 // Task represents a task
 type Task struct {
-	Task         string
-	Cmds         []*Cmd
-	Deps         []*Dep
-	Desc         string
-	Summary      string
-	Sources      []string
-	Generates    []string
-	Status       []string
+	Task          string
+	Cmds          []*Cmd
+	Deps          []*Dep
+	Desc          string
+	Summary       string
+	Sources       []string
+	Generates     []string
+	Status        []string
 	Preconditions []*Precondition
-	Dir          string
-	Vars         Vars
-	Env          Vars
-	Silent       bool
-	Method       string
-	Prefix       string
-	IgnoreError  bool `yaml:"ignore_error"`
+	Dir           string
+	Vars          Vars
+	Env           Vars
+	Silent        bool
+	Method        string
+	Prefix        string
+	IgnoreError   bool `yaml:"ignore_error"`
 }
