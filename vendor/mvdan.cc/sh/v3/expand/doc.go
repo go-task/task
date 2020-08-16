49bf395f61275977e0c3bdd0403da35a5ff4dd25
Andrey Nering <andrey.nering@gmail.com>
2020-08-16 15:46:22 -0300

    Remove the vendor/ directory

--- vendor/mvdan.cc/sh/v3/expand/doc.go
+++
@@ -1,5 +0,0 @@
-// Copyright (c) 2018, Daniel Martí <mvdan@mvdan.cc>
-// See LICENSE for licensing information
-
-// Package expand contains code to perform various shell expansions.
-package expand
