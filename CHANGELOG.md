8d26e34b0ab5b952d99b9b035d3bccdf67cb4dc4
Andrey Nering <andrey@nering.com.br>
2022-07-16 22:07:24 -0300

    Use `--list-all` to PowerShell completion + Add CHANGELOG to #803

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 # Changelog
 
+## Unreleased
+
+- Fix ZSH and PowerShell completions to consider all tasks instead of just the
+  public ones (those with descriptions)
+  ([#803](https://github.com/go-task/task/pull/803)).
+
 ## v3.14.0 - 2022-07-08
 
 - Add ability to override the `.task` directory location with the
--- completion/ps/task.ps1
+++ completion/ps/task.ps1
@@ -3,7 +3,7 @@ $scriptBlock = {
 	$curReg = "task{.exe}? (.*?)$"
 	$startsWith = $wordToComplete | Select-String $curReg -AllMatches | ForEach-Object { $_.Matches.Groups[1].Value }
 	$reg = "\* ($startsWith.+?):"
-	$listOutput = $(task -l)
+	$listOutput = $(task --list-all)
 	$listOutput | Select-String $reg -AllMatches | ForEach-Object { $_.Matches.Groups[1].Value + " " }
 }
 
