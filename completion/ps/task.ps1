2e573d37aee2d677eb398a9686f3df1bee79f473
Trim21 <trim21me@gmail.com>
2022-10-18 00:09:07 +0800

    Update task.ps1

--- completion/ps/task.ps1
+++ completion/ps/task.ps1
@@ -2,7 +2,7 @@ $scriptBlock = {
 	param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameters )
 	$reg = "\* ($commandName.+?):"
 	$listOutput = $(task --list-all)
-	$listOutput | Select-String $reg -AllMatches | ForEach-Object { $_.Matches.Groups[1].Value + " " }
+	$listOutput | Select-String $reg -AllMatches | ForEach-Object { $_.Matches.Groups[1].Value }
 }
 
 Register-ArgumentCompleter -CommandName task -ScriptBlock $scriptBlock
