9decabfd0d9329b66c31693ebaea8808701947cd
Trim21 <trim21me@gmail.com>
2023-05-16 05:53:42 +0800

    Update task.ps1

--- completion/ps/task.ps1
+++ completion/ps/task.ps1
@@ -1,10 +1,30 @@
-$scriptBlock = {
+Register-ArgumentCompleter -CommandName task -ScriptBlock {
 	param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameters)
-		$listOutput = $(task --list-all --silent)
-		$reg = "^($commandName.*?)$"
-		$listOutput | Select-String $reg -AllMatches | ForEach-Object {
-			 $_.Matches.Groups[1].Value;
-		}
+		if ($commandName -match '^-{1,2}[^-]*$') {
+			$listOutput = @(
+				'--concurrency=',
+				'--interval=',
+				'--output=interleaved',
+				'--output=group',
+				'--output=prefixed',
+				'--color',
+				'--dry',
+				'--force',
+				'--parallel',
+				'--silent',
+				'--status',
+				'--verbose',
+				'--watch'
+			)
+			$reg = "^($commandName.*?)$"
+			$listOutput | Select-String $reg -AllMatches | ForEach-Object {
+				$_.Matches.Groups[1].Value;
+			}
+		} else {
+			$listOutput = $(task --list-all --silent)
+			$reg = "^($commandName.*?)$"
+			$listOutput | Select-String $reg -AllMatches | ForEach-Object {
+				$_.Matches.Groups[1].Value;
+			}
+	}
 }
-
-Register-ArgumentCompleter -CommandName task -ScriptBlock $scriptBlock
