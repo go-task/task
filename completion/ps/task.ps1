b4ce2f177803424c8d4fc74e7c4015a7e92b1901
Trim21 <trim21me@gmail.com>
2023-05-16 07:00:41 +0800

    Update task.ps1

--- completion/ps/task.ps1
+++ completion/ps/task.ps1
@@ -1,21 +1,25 @@
+using namespace System.Management.Automation
+
 Register-ArgumentCompleter -CommandName task -ScriptBlock {
 	param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameters)
-		if ($commandName -match '^-{1,2}[^-]*$') {
-			return @(
-				'--concurrency=',
-				'--interval=',
-				'--output=interleaved',
-				'--output=group',
-				'--output=prefixed',
-				'--color',
-				'--dry',
-				'--force',
-				'--parallel',
-				'--silent',
-				'--status',
-				'--verbose',
-				'--watch'
-			) | Where-Object { $_ -like "$commandName*" } | ForEach-Object { $_ }
+		if ($commandName.StartsWith('-')) {
+			$completions = @(
+				[CompletionResult]::new('--color ', '--color', [CompletionResultType]::ParameterName, 'Colored output.'),
+				[CompletionResult]::new('--concurrency=', '--concurrency=', [CompletionResultType]::ParameterName, 'concurrency'),
+				[CompletionResult]::new('--interval=', '--interval=', [CompletionResultType]::ParameterName, 'interval'),
+				[CompletionResult]::new('--output=interleaved ', '--output=interleaved', [CompletionResultType]::ParameterName, 'output style'),
+				[CompletionResult]::new('--output=group ', '--output=group', [CompletionResultType]::ParameterName, 'output style'),
+				[CompletionResult]::new('--output=prefixed ', '--output=prefixed', [CompletionResultType]::ParameterName, 'output style'),
+				[CompletionResult]::new('--dry ', '--dry', [CompletionResultType]::ParameterName, '--dry'),
+				[CompletionResult]::new('--force ', '--force', [CompletionResultType]::ParameterName, '--force'),
+				[CompletionResult]::new('--parallel ', '--parallel', [CompletionResultType]::ParameterName, '--parallel'),
+				[CompletionResult]::new('--silent ', '--silent', [CompletionResultType]::ParameterName, '--silent'),
+				[CompletionResult]::new('--status ', '--status', [CompletionResultType]::ParameterName, '--status'),
+				[CompletionResult]::new('--verbose ', '--verbose', [CompletionResultType]::ParameterName, '--verbose'),
+				[CompletionResult]::new('--watch ', '--watch', [CompletionResultType]::ParameterName,'--watch')
+			)
+
+			return $completions.Where{ $_.CompletionText -like "$commandName*" }
 		}
 
 		return $(task --list-all --silent) | Where-Object { $_ -like "$commandName*" } | ForEach-Object { $_ }
