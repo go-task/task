e2c1b3b9314eeafee519d493433addea1ce0c70a
Tim De Pauw <timdp@users.noreply.github.com>
2023-06-11 03:08:28 +0200

    feat: add support for single cmd task syntax (#1131)

--- task_test.go
+++ task_test.go
@@ -2013,6 +2013,18 @@ func TestSplitArgs(t *testing.T) {
 	assert.Equal(t, "3\n", buff.String())
 }
 
+func TestSingleCmdDep(t *testing.T) {
+	tt := fileContentTest{
+		Dir:    "testdata/single_cmd_dep",
+		Target: "foo",
+		Files: map[string]string{
+			"foo.txt": "foo\n",
+			"bar.txt": "bar\n",
+		},
+	}
+	tt.Run(t)
+}
+
 func TestSilence(t *testing.T) {
 	var buff bytes.Buffer
 	e := task.Executor{
