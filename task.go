c716c936d23268b2801d207d461560e77499ddce
Pete Davison <pd93.uk@outlook.com>
2023-12-02 02:26:08 +0000

    fix: handle errors when sh is used in Taskfiles with the any variables experiment enabled

--- task.go
+++ task.go
@@ -478,8 +478,8 @@ func (e *Executor) GetTaskList(filters ...FilterFunc) ([]*taskfile.Task, error) 
 		task := tasks[idx]
 		g.Go(func() error {
 			compiledTask, err := e.FastCompiledTask(taskfile.Call{Task: task.Task})
-			if err == nil {
-				task = compiledTask
+			if err != nil {
+				return err
 			}
 			tasks[idx] = compiledTask
 			return nil
