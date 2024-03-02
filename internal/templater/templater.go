a27eba434003490a67297fe4448e2d9cab9c56d6
Pete Davison <pd93.uk@outlook.com>
2024-03-02 23:59:46 +0000

    feat: move traverser to deepcopy package

--- internal/templater/templater.go
+++ internal/templater/templater.go
@@ -3,10 +3,10 @@ package templater
 import (
 	"bytes"
 	"maps"
-	"reflect"
 	"strings"
 	"text/template"
 
+	"github.com/go-task/task/v3/internal/deepcopy"
 	"github.com/go-task/task/v3/taskfile/ast"
 )
 
@@ -51,16 +51,24 @@ func ReplaceWithExtra[T any](v T, cache *Cache, extra map[string]any) T {
 		maps.Copy(data, extra)
 	}
 
-	original := reflect.ValueOf(&v)
-	copy := reflect.New(original.Type()).Elem()
-
-	// Replace the variables in the value
-	if err := replaceValue(copy, original, data); err != nil {
+	// Traverse the value and parse any template variables
+	copy, err := deepcopy.TraverseStringsFunc(v, func(v string) (string, error) {
+		tpl, err := template.New("").Funcs(templateFuncs).Parse(v)
+		if err != nil {
+			return v, err
+		}
+		var b bytes.Buffer
+		if err := tpl.Execute(&b, data); err != nil {
+			return v, err
+		}
+		return strings.ReplaceAll(b.String(), "<no value>", ""), nil
+	})
+	if err != nil {
 		cache.err = err
 		return v
 	}
 
-	return copy.Elem().Interface().(T)
+	return copy
 }
 
 func ReplaceGlobs(globs []*ast.Glob, cache *Cache) []*ast.Glob {
@@ -104,93 +112,4 @@ func ReplaceVarsWithExtra(vars *ast.Vars, cache *Cache, extra map[string]any) *a
 	})
 
 	return &newVars
-}
-
-// replaceValue is a recursive function that replaces all the string template
-// variables in the given value with the values in the data map. If the value is
-// a string, the function will replace the variables in the string and return
-// the new string. If the value is a struct or a slice, the function will
-// recursively call itself for each field or element of the struct or slice
-// until all strings inside the struct or slice are replaced.
-func replaceValue(copy, v reflect.Value, data map[string]any) error {
-	switch v.Kind() {
-
-	case reflect.Ptr:
-		// Unwrap the pointer
-		originalValue := v.Elem()
-		// If the pointer is nil, do nothing
-		if !originalValue.IsValid() {
-			return nil
-		}
-		// Create an empty copy from the original value's type
-		copy.Set(reflect.New(originalValue.Type()))
-		// Unwrap the newly created pointer and call replaceValue recursively
-		if err := replaceValue(copy.Elem(), originalValue, data); err != nil {
-			return err
-		}
-
-	case reflect.Interface:
-		// Unwrap the interface
-		originalValue := v.Elem()
-		// Create an empty copy from the original value's type
-		copyValue := reflect.New(originalValue.Type())
-		// Unwrap the newly created pointer and call replaceValue recursively
-		if err := replaceValue(copyValue.Elem(), originalValue, data); err != nil {
-			return err
-		}
-		copy.Set(copyValue)
-
-	case reflect.Struct:
-		// Loop over each field and call replaceValue recursively
-		for i := 0; i < v.NumField(); i += 1 {
-			if err := replaceValue(copy.Field(i), v.Field(i), data); err != nil {
-				return err
-			}
-		}
-
-	case reflect.Slice:
-		// Create an empty copy from the original value's type
-		copy.Set(reflect.MakeSlice(v.Type(), v.Len(), v.Cap()))
-		// Loop over each element and call replaceValue recursively
-		for i := 0; i < v.Len(); i += 1 {
-			if err := replaceValue(copy.Index(i), v.Index(i), data); err != nil {
-				return err
-			}
-		}
-
-	case reflect.Map:
-		// Create an empty copy from the original value's type
-		copy.Set(reflect.MakeMap(v.Type()))
-		// Loop over each key
-		for _, key := range v.MapKeys() {
-			// Create a copy of each map index
-			originalValue := v.MapIndex(key)
-			if originalValue.IsNil() {
-				continue
-			}
-			copyValue := reflect.New(originalValue.Type()).Elem()
-			// Call replaceValue recursively
-			if err := replaceValue(copyValue, originalValue, data); err != nil {
-				return err
-			}
-			copy.SetMapIndex(key, copyValue)
-		}
-
-	case reflect.String:
-		tpl, err := template.New("").Funcs(templateFuncs).Parse(v.String())
-		if err != nil {
-			return err
-		}
-		var b bytes.Buffer
-		if err := tpl.Execute(&b, data); err != nil {
-			return err
-		}
-		str := strings.ReplaceAll(b.String(), "<no value>", "")
-		copy.SetString(str)
-
-	default:
-		copy.Set(v)
-	}
-
-	return nil
 }
