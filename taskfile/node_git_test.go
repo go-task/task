362c6920df8b67e340a9095cda246e1889fbf0cb
Valentin Maerten <maerten.valentin@gmail.com>
2024-05-10 22:54:57 +0200

    add tests

--- taskfile/node_git_test.go
+++ taskfile/node_git_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func TestGitNode(t *testing.T) {
+func TestGitNode_ssh(t *testing.T) {
 	node, err := NewGitNode("git@github.com:foo/bar.git?ref=main//Taskfile.yml", "", false)
 	assert.NoError(t, err)
 	assert.Equal(t, "main", node.ref)
@@ -17,20 +17,38 @@ func TestGitNode(t *testing.T) {
 	assert.Equal(t, "ssh://git@github.com/foo/bar.git?ref=main//common.yml", entrypoint)
 }
 
-func TestGitNode_WithoutRef(t *testing.T) {
-	node, err := NewGitNode("git@github.com/foo/bar?ref=main//Taskfile.yml", "", false)
+func TestGitNode_sshWithDir(t *testing.T) {
+	node, err := NewGitNode("git@github.com:foo/bar.git?ref=main//directory/Taskfile.yml", "", false)
 	assert.NoError(t, err)
 	assert.Equal(t, "main", node.ref)
-	assert.Equal(t, "Taskfile.yml", node.path)
-	assert.Equal(t, "github.com/foo/bar?ref=main//Taskfile.yml", node.rawUrl)
-	assert.Equal(t, "github.com/foo/bar", node.URL.String())
+	assert.Equal(t, "directory/Taskfile.yml", node.path)
+	assert.Equal(t, "ssh://git@github.com/foo/bar.git?ref=main//directory/Taskfile.yml", node.rawUrl)
+	assert.Equal(t, "ssh://git@github.com/foo/bar.git", node.URL.String())
+	entrypoint, err := node.ResolveEntrypoint("common.yml")
+	assert.NoError(t, err)
+	assert.Equal(t, "ssh://git@github.com/foo/bar.git?ref=main//directory/common.yml", entrypoint)
 }
 
-func TestGitNode_WithoutPath(t *testing.T) {
-	node, err := NewGitNode("git@github.com/foo/bar?ref=main//Taskfile.yml", "", false)
+func TestGitNode_https(t *testing.T) {
+	node, err := NewGitNode("https://github.com/foo/bar.git?ref=main//Taskfile.yml", "", false)
 	assert.NoError(t, err)
 	assert.Equal(t, "main", node.ref)
 	assert.Equal(t, "Taskfile.yml", node.path)
-	assert.Equal(t, "github.com/foo/bar?ref=main//Taskfile.yml", node.rawUrl)
-	assert.Equal(t, "github.com/foo/bar", node.URL.String())
+	assert.Equal(t, "https://github.com/foo/bar.git?ref=main//Taskfile.yml", node.rawUrl)
+	assert.Equal(t, "https://github.com/foo/bar.git", node.URL.String())
+	entrypoint, err := node.ResolveEntrypoint("common.yml")
+	assert.NoError(t, err)
+	assert.Equal(t, "https://github.com/foo/bar.git?ref=main//common.yml", entrypoint)
+}
+
+func TestGitNode_httpsWithDir(t *testing.T) {
+	node, err := NewGitNode("https://github.com/foo/bar.git?ref=main//directory/Taskfile.yml", "", false)
+	assert.NoError(t, err)
+	assert.Equal(t, "main", node.ref)
+	assert.Equal(t, "directory/Taskfile.yml", node.path)
+	assert.Equal(t, "https://github.com/foo/bar.git?ref=main//directory/Taskfile.yml", node.rawUrl)
+	assert.Equal(t, "https://github.com/foo/bar.git", node.URL.String())
+	entrypoint, err := node.ResolveEntrypoint("common.yml")
+	assert.NoError(t, err)
+	assert.Equal(t, "https://github.com/foo/bar.git?ref=main//directory/common.yml", entrypoint)
 }
