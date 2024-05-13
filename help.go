f5826d9711bd2b29b6083fa960af7404d5bb3239
Valentin Maerten <maerten.valentin@gmail.com>
2024-05-13 13:38:40 +0200

    feat: display all lines aligned  in a table

--- help.go
+++ help.go
@@ -105,8 +105,18 @@ func (e *Executor) ListTasks(o ListOptions) (bool, error) {
 	for _, task := range tasks {
 		e.Logger.FOutf(w, logger.Yellow, "* ")
 		e.Logger.FOutf(w, logger.Green, task.Task)
-		desc := strings.ReplaceAll(task.Desc, "\n", " ")
-		e.Logger.FOutf(w, logger.Default, ": \t%s", desc)
+		desc := strings.Split(strings.TrimSuffix(task.Desc, "\n"), "\n")
+
+		e.Logger.FOutf(w, logger.Default, ": \t%s", desc[0])
+
+		for _, d := range desc[1:] {
+			_, _ = fmt.Fprint(w, "\n")
+			//We need to keep the same color as if it was a new task to keep the output consistent
+			// because tabwriter cannot handle color codes
+			e.Logger.FOutf(w, logger.Yellow, "")
+			e.Logger.FOutf(w, logger.Green, "")
+			e.Logger.FOutf(w, logger.Default, "\t%s", d)
+		}
 		if len(task.Aliases) > 0 {
 			e.Logger.FOutf(w, logger.Cyan, "\t(aliases: %s)", strings.Join(task.Aliases, ", "))
 		}
