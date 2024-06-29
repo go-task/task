8a41ca3a47d6cd8683e9e6c598ae96986d24467d
Trim21 <trim21.me@gmail.com>
2024-06-29 15:00:56 +0800

    fix

--- taskfile/ast/tasks.go
+++ taskfile/ast/tasks.go
@@ -52,7 +52,7 @@ func (t *Tasks) FindMatchingTasks(call *Call) []*MatchingTask {
 	return matchingTasks
 }
 
-func (t1 *Tasks) Merge(t2 Tasks, include *Include, includedTaskfileVars *Vars) {
+func (t1 *Tasks) Merge(t2 *Tasks, include *Include, includedTaskfileVars *Vars) {
 	_ = t2.Range(func(name string, v *Task) error {
 		// We do a deep copy of the task struct here to ensure that no data can
 		// be changed elsewhere once the taskfile is merged.
