392fb90f37df744667d021e435a34f3a040a26f1
James Lucktaylor <jlucktay+github@gmail.com>
2024-06-30 17:22:32 +0100

    test(taskfile): add coverage around new TTL functionality in Cache

--- taskfile/cache_test.go
+++ taskfile/cache_test.go
@@ -2,6 +2,7 @@ package taskfile_test
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 
@@ -11,5 +12,11 @@ import (
 func TestNewCache(t *testing.T) {
 	cacheDir := t.TempDir()
 	_, err := taskfile.NewCache(cacheDir)
+	require.NoErrorf(t, err, "creating new cache in temporary directory '%s'", cacheDir)
+}
+
+func TestNewCacheWithTTL(t *testing.T) {
+	cacheDir := t.TempDir()
+	_, err := taskfile.NewCache(cacheDir, taskfile.WithTTL(time.Hour))
 	require.NoErrorf(t, err, "creating new cache in temporary directory '%s'", cacheDir)
 }
