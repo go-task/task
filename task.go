d19fdd8d56b4d6fad8094e1aaf612efa04eaed20
Pete Davison <pd93.uk@outlook.com>
2024-08-12 15:44:40 +0000

    refactor: loop variables

--- task.go
+++ task.go
@@ -494,14 +494,12 @@ func (e *Executor) GetTaskList(filters ...FilterFunc) ([]*ast.Task, error) {
 
 	// Compile the list of tasks
 	for i := range tasks {
-		idx := i
-		task := tasks[idx]
 		g.Go(func() error {
-			compiledTask, err := e.FastCompiledTask(&ast.Call{Task: task.Task})
+			compiledTask, err := e.FastCompiledTask(&ast.Call{Task: tasks[i].Task})
 			if err != nil {
 				return err
 			}
-			tasks[idx] = compiledTask
+			tasks[i] = compiledTask
 			return nil
 		})
 	}
