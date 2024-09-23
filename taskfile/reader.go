46ad22191f14899bc25e2c00976ae463b5be09ea
Valentin Maerten <maerten.valentin@gmail.com>
2024-09-23 21:22:38 +0200

    fix(remote: wait for prompt in the reader

--- taskfile/reader.go
+++ taskfile/reader.go
@@ -244,11 +244,13 @@ func (r *Reader) readNode(node Node) (*ast.Taskfile, error) {
 				// If there is a cached hash, but it doesn't match the expected hash, prompt the user to continue
 				prompt = fmt.Sprintf(taskfileChangedPrompt, node.Location())
 			}
+			r.graph.Lock()
 			if prompt != "" {
 				if err := r.logger.Prompt(logger.Yellow, prompt, "n", "y", "yes"); err != nil {
 					return nil, &errors.TaskfileNotTrustedError{URI: node.Location()}
 				}
 			}
+			r.graph.Unlock()
 
 			// If the hash has changed (or is new)
 			if checksum != cachedChecksum {
