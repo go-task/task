b7cf0b9c989612a3aa284a43c62e9e2e1f505c72
Matheus Mina <mfbmina@gmail.com>
2024-10-17 13:50:05 -0300

    apply review

--- taskfile/ast/prompt.go
+++ taskfile/ast/prompt.go
@@ -1,21 +1,29 @@
 package ast
 
+import (
+	"gopkg.in/yaml.v3"
+
+	"github.com/go-task/task/v3/errors"
+)
+
 type Prompt []string
 
-func (p *Prompt) UnmarshalYAML(unmarshal func(any) error) error {
-	var m []string
-	err := unmarshal(&m)
-	if err == nil {
-		*p = m
+func (p *Prompt) UnmarshalYAML(node *yaml.Node) error {
+	switch node.Kind {
+	case yaml.ScalarNode:
+		var str string
+		if err := node.Decode(&str); err != nil {
+			return errors.NewTaskfileDecodeError(err, node)
+		}
+		*p = []string{str}
+		return nil
+	case yaml.SequenceNode:
+		var list []string
+		if err := node.Decode(&list); err != nil {
+			return errors.NewTaskfileDecodeError(err, node)
+		}
+		*p = list
 		return nil
 	}
-
-	var s string
-	err = unmarshal(&s)
-	if err != nil {
-		return err
-	}
-
-	*p = []string{s}
-	return nil
+	return errors.NewTaskfileDecodeError(nil, node).WithTypeMessage("prompt")
 }
