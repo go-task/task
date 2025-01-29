53ab1e87e8b111aae429ed5f5a78e96e6facc323
HeCorr <75134774+HeCorr@users.noreply.github.com>
2025-01-28 22:36:19 -0300

    move TestInit into init_test.go file
    as requested by pd93.

---
+++ init_test.go
@@ -0,0 +1,31 @@
+package task_test
+
+import (
+	"io"
+	"os"
+	"testing"
+
+	"github.com/go-task/task/v3"
+	"github.com/go-task/task/v3/internal/filepathext"
+)
+
+func TestInit(t *testing.T) {
+	t.Parallel()
+
+	const dir = "testdata/init"
+	file := filepathext.SmartJoin(dir, "Taskfile.yml")
+
+	_ = os.Remove(file)
+	if _, err := os.Stat(file); err == nil {
+		t.Errorf("Taskfile.yml should not exist")
+	}
+
+	if err := task.InitTaskfile(io.Discard, dir); err != nil {
+		t.Error(err)
+	}
+
+	if _, err := os.Stat(file); err != nil {
+		t.Errorf("Taskfile.yml should exist")
+	}
+	_ = os.Remove(file)
+}
