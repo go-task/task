31fc45dc7d0da1ea3c1b45433a5d4e51010e6917
Valentin Maerten <maerten.valentin@gmail.com>
2025-04-21 16:53:02 +0200

    docs: add since badge
    wip
    
    wip
    
    wip

---
+++ website/src/components/AvailableSince.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { FaCalendarCheck } from 'react-icons/fa';
+import { useColorMode } from '@docusaurus/theme-common';
+
+type Props = {
+	version: string;
+};
+
+export default function AvailableSince({ version }: Props) {
+	const { colorMode } = useColorMode();
+
+	const isDark = colorMode === 'dark';
+
+	const style = {
+		display: 'inline-flex',
+		alignItems: 'center',
+		border: `1px solid var(--ifm-color-primary)`,
+		color: isDark ? '#fffdf9' : 'var(--ifm-color-primary)',
+		padding: '0.2em 0.6em',
+		borderRadius: '999px',
+		fontSize: '0.8em',
+		fontWeight: 500,
+		backgroundColor: isDark ? 'var(--ifm-color-primary)' : '#f0fdfa',
+		marginBottom:  'calc(var(--ifm-heading-vertical-rhythm-bottom) * var(--ifm-leading))',
+	};
+
+
+	return (
+		<span className="available-since-badge" style={style}>
+      <FaCalendarCheck style={{ marginRight: '0.4em' }} />
+      Since {version}
+    </span>
+	);
+}
+
+
