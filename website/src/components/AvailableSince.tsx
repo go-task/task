9d67d2688d7419b973a12958a38ca9a533295d34
Valentin Maerten <maerten.valentin@gmail.com>
2025-04-22 14:09:32 +0200

    apply prettier

--- website/src/components/AvailableSince.tsx
+++ website/src/components/AvailableSince.tsx
@@ -3,34 +3,32 @@ import { FaCalendarCheck } from 'react-icons/fa';
 import { useColorMode } from '@docusaurus/theme-common';
 
 type Props = {
-	version: string;
+  version: string;
 };
 
 export default function AvailableSince({ version }: Props) {
-	const { colorMode } = useColorMode();
+  const { colorMode } = useColorMode();
 
-	const isDark = colorMode === 'dark';
+  const isDark = colorMode === 'dark';
 
-	const style = {
-		display: 'inline-flex',
-		alignItems: 'center',
-		border: `1px solid var(--ifm-color-primary)`,
-		color: isDark ? '#fffdf9' : 'var(--ifm-color-primary)',
-		padding: '0.2em 0.6em',
-		borderRadius: '999px',
-		fontSize: '0.8em',
-		fontWeight: 500,
-		backgroundColor: isDark ? 'var(--ifm-color-primary)' : '#f0fdfa',
-		marginBottom:  'calc(var(--ifm-heading-vertical-rhythm-bottom) * var(--ifm-leading))',
-	};
+  const style = {
+    display: 'inline-flex',
+    alignItems: 'center',
+    border: `1px solid var(--ifm-color-primary)`,
+    color: isDark ? '#fffdf9' : 'var(--ifm-color-primary)',
+    padding: '0.2em 0.6em',
+    borderRadius: '999px',
+    fontSize: '0.8em',
+    fontWeight: 500,
+    backgroundColor: isDark ? 'var(--ifm-color-primary)' : '#f0fdfa',
+    marginBottom:
+      'calc(var(--ifm-heading-vertical-rhythm-bottom) * var(--ifm-leading))'
+  };
 
-
-	return (
-		<span className="available-since-badge" style={style}>
+  return (
+    <span className="available-since-badge" style={style}>
       <FaCalendarCheck style={{ marginRight: '0.4em' }} />
       Since {version}
     </span>
-	);
+  );
 }
-
-
