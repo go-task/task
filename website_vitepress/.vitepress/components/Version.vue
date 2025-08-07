2863d1ecc8a392d16a7c63e611f902e0b2e66e27
Valentin Maerten <maerten.valentin@gmail.com>
2025-08-07 11:55:23 +0200

    remove old website

--- website_vitepress/.vitepress/components/Version.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-import { VPBadge } from 'vitepress/theme'
-</script>
-
-<template>
-  <VPBadge type="info">
-    <slot />+
-  </VPBadge>
-</template>
--- website_vitepress/.vitepress/meta.ts
+++
@@ -1,5 +0,0 @@
-export const taskName = 'Task';
-export const taskDescription =
-  'A fast, cross-platform build tool inspired by Make, designed for modern workflows.';
-
-export const ogUrl = 'https://taskfile.dev/';
