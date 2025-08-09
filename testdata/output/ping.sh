050d8777bd636c4a1752f3ff66b9a229179a6404
Timothy Rule <34501912+trulede@users.noreply.github.com>
2025-08-09 23:32:52 +0200

    Improve test bash script behavior.

--- testdata/output/ping.sh
+++ testdata/output/ping.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
+ready=0
 count=0
 trap sigusr1_handler SIGUSR1
+trap sigusr2_handler SIGUSR2
 
 function sigusr1_handler() {
-    stdbuf -o0 echo "PONG $((++count))"
-    stdbuf -o0 kill -SIGUSR1 $pids 2>/dev/null
+    ((ready = 1))
+}
+
+function sigusr2_handler() {
+    ((count++))
+    stdbuf -o0 echo "PONG $count"
+    if [ $count -lt 10 ]; then
+        stdbuf -o0 kill -SIGUSR2 $pids 2>/dev/null
+    fi
 }
 
 while [[ -z $pids ]]
@@ -12,8 +21,14 @@ do
     pids=$(ps -ef | grep pong.sh | grep -v grep | awk '{print $2}')
     sleep 0.001
 done
- 
-kill -SIGUSR1 $pids 2>/dev/null
+
+while [[ $ready == 0 ]]
+do
+    stdbuf -o0 kill -SIGUSR1 $pids 2>/dev/null
+    sleep 0.001
+done
+
+stdbuf -o0 kill -SIGUSR2 $pids 2>/dev/null
 while [[ $count -lt 10 ]]; do
     sleep 0.001
 done
