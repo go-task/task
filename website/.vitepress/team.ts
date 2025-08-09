8c58ffe1302b1e52b7c9aa782a4c06395891aa74
Andrey Nering <andreynering@users.noreply.github.com>
2025-08-09 17:49:38 -0300

    chore: sync `team.ts` with `team.md`

--- website/.vitepress/team.ts
+++ website/.vitepress/team.ts
@@ -2,29 +2,35 @@ export const team = [
   {
     slug: 'andreynering',
     avatar: 'https://www.github.com/andreynering.png',
-    name: 'Andrey Nering',
+    name: '🇧🇷 Andrey Nering',
     title: 'Creator & Maintainer',
     sponsor: 'https://github.com/sponsors/andreynering',
     links: [
       { icon: 'github', link: 'https://github.com/andreynering' },
-      { icon: 'x', link: 'https://x.com/andreynering' }
+      { icon: 'discord', link: 'https://discord.com/users/310141681926275082' },
+      { icon: 'x', link: 'https://x.com/andreynering' },
+      {
+        icon: 'bluesky',
+        link: 'https://bsky.app/profile/andreynering.bsky.social'
+      },
+      { icon: 'mastodon', link: 'https://mastodon.social/@andreynering' }
     ]
   },
   {
     slug: 'pd93',
     avatar: 'https://www.github.com/pd93.png',
-    name: 'Pete Davison',
+    name: '🇬🇧 Pete Davison',
     title: 'Maintainer',
     sponsor: 'https://github.com/sponsors/pd93',
     links: [
       { icon: 'github', link: 'https://github.com/pd93' },
-      { icon: 'x', link: 'https://x.com/youyuxi' }
+      { icon: 'bluesky', link: 'https://bsky.app/profile/pd93.uk' }
     ]
   },
   {
     slug: 'vmaerten',
     avatar: 'https://www.github.com/vmaerten.png',
-    name: 'Valentin Maerten',
+    name: '🇫🇷 Valentin Maerten',
     title: 'Maintainer',
     sponsor: 'https://github.com/sponsors/vmaerten',
     links: [
