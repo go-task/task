4d90514f360837b0b66f71054c792c8f7475dfe6
Timothy Rule (VM/EMT3) <Timothy.Rule@de.bosch.com>
2025-11-05 14:02:42 +0100

    E2E additional testscript commands.

--- cmd/task/task_test.go
+++ cmd/task/task_test.go
@@ -5,7 +5,9 @@ package main
 
 import (
 	"os"
+	"path/filepath"
 	"testing"
+	"time"
 
 	"github.com/rogpeppe/go-internal/testscript"
 )
@@ -18,8 +20,40 @@ func TestMain(m *testing.M) {
 
 func TestE2E(t *testing.T) {
 	testscript.Run(t, testscript.Params{
-		Dir:   "testdata",
-		Cmds:  map[string]func(ts *testscript.TestScript, neg bool, args []string){},
+		Dir: "testdata",
+		Cmds: map[string]func(ts *testscript.TestScript, neg bool, args []string){
+			"sleep": sleep,
+			"touch": touch,
+		},
 		Setup: func(e *testscript.Env) error { return nil },
 	})
+}
+
+func sleep(ts *testscript.TestScript, neg bool, args []string) {
+	duration := time.Second
+	if len(args) == 1 {
+		d, err := time.ParseDuration(args[0])
+		ts.Check(err)
+		duration = d
+	}
+	time.Sleep(duration)
+}
+
+func touch(ts *testscript.TestScript, neg bool, args []string) {
+	if len(args) != 1 {
+		ts.Fatalf("touch <file>")
+	}
+	// Get the relative path to the scripts current directory.
+	path := ts.MkAbs(args[0])
+	// Create the file (if necessary).
+	err := os.MkdirAll(filepath.Dir(path), 0750)
+	ts.Check(err)
+	file, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE, 0644)
+	ts.Check(err)
+	err = file.Close()
+	ts.Check(err)
+	// Now update the timestamp.
+	t := time.Now()
+	err = os.Chtimes(path, t, t)
+	ts.Check(err)
 }
