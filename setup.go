1deb1d8195ea788e83567adec8263f2b41208d2e
Valentin Maerten <maerten.valentin@gmail.com>
2025-11-20 19:49:02 +0100

    fix: disable fuzzy

--- setup.go
+++ setup.go
@@ -103,24 +103,25 @@ func (e *Executor) readTaskfile(node taskfile.Node) error {
 }
 
 func (e *Executor) setupFuzzyModel() {
+
 	if e.Taskfile == nil {
 		return
 	}
-
-	model := fuzzy.NewModel()
-	model.SetThreshold(1) // because we want to build grammar based on every task name
-
-	var words []string
-	for name, task := range e.Taskfile.Tasks.All(nil) {
-		if task.Internal {
-			continue
-		}
-		words = append(words, name)
-		words = slices.Concat(words, task.Aliases)
-	}
-
-	model.Train(words)
-	e.fuzzyModel = model
+	//
+	//model := fuzzy.NewModel()
+	//model.SetThreshold(1) // because we want to build grammar based on every task name
+	//
+	//var words []string
+	//for name, task := range e.Taskfile.Tasks.All(nil) {
+	//	if task.Internal {
+	//		continue
+	//	}
+	//	words = append(words, name)
+	//	words = slices.Concat(words, task.Aliases)
+	//}
+	//
+	//model.Train(words)
+	//e.fuzzyModel = model
 }
 
 func (e *Executor) setupTempDir() error {
