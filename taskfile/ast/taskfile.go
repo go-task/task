74a324ad4b2b31894f5c4e488111dfba4ad5f85d
Timothy Rule <34501912+trulede@users.noreply.github.com>
2025-12-06 10:02:41 +0100

    wip

--- taskfile/ast/taskfile.go
+++ taskfile/ast/taskfile.go
@@ -61,6 +61,11 @@ func (t1 *Taskfile) Merge(t2 *Taskfile, include *Include) error {
 	}
 	t1.Vars.Merge(t2.Vars, include)
 	t1.Env.Merge(t2.Env, include)
+	if t2.Run != "" {
+		for _, v := range t2.Tasks.All(nil) {
+			v.Run = t2.Run
+		}
+	}
 	return t1.Tasks.Merge(t2.Tasks, include, t1.Vars)
 }
 
