92d397caeaaa3d7e15643e833fca59bcf8aa4368
Timothy Rule <34501912+trulede@users.noreply.github.com>
2026-01-18 15:49:10 +0100

    Add a check for stdout.

--- cmd/task/console_win.go
+++ cmd/task/console_win.go
@@ -14,9 +14,11 @@ func init() {
 	// Ensure that Windows console handles ANSI escape-codes correctly.
 	if runtime.GOOS == "windows" {
 		stdout := windows.Handle(os.Stdout.Fd())
-		var originalMode uint32
-		if err := windows.GetConsoleMode(stdout, &originalMode); err == nil {
-			windows.SetConsoleMode(stdout, originalMode|windows.ENABLE_VIRTUAL_TERMINAL_PROCESSING)
+		if stdout != 0 {
+			var originalMode uint32
+			if err := windows.GetConsoleMode(stdout, &originalMode); err == nil {
+				windows.SetConsoleMode(stdout, originalMode|windows.ENABLE_VIRTUAL_TERMINAL_PROCESSING)
+			}
 		}
 	}
 }
