58cb143d5bec1b444ee8c8044dd78d7f69f6963a
Valentin Maerten <maerten.valentin@gmail.com>
2026-01-25 15:24:40 +0100

    fix(summary): adapt to new enum struct with ref support

--- internal/summary/summary.go
+++ internal/summary/summary.go
@@ -248,10 +248,10 @@ func printTaskRequires(l *logger.Logger, t *ast.Task) {
 
 	for _, v := range t.Requires.Vars {
 		// If the variable has enum constraints, format accordingly
-		if len(v.Enum) > 0 {
+		if v.Enum != nil && len(v.Enum.Value) > 0 {
 			l.Outf(logger.Yellow, "    - %s:\n", v.Name)
 			l.Outf(logger.Yellow, "        enum:\n")
-			for _, enumValue := range v.Enum {
+			for _, enumValue := range v.Enum.Value {
 				l.Outf(logger.Yellow, "          - %s\n", enumValue)
 			}
 		} else {
